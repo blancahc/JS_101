3c3535d24e799c35c99955554f27d4705a616673
Blanca Hernandez <blamca1029@gmail.com>
2020-03-20 22:16:29 -0700

    refactored and fixed errors

--- lesson_2/mortgage_loan_calculator.js
+++ lesson_2/mortgage_loan_calculator.js
@@ -1,56 +1,88 @@
 const MESSAGES = require('./mortgage_messages.json');
 const readline = require('readline-sync');
 
-
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
 function invalidNumber(number) {
-  return number.trimStart() === '' || Number.isNaN(Number(number)) || number < 0;
+  //return number.trim() === '' || Number.isNaN(number) || number <= 0;
+  return Number.isNaN(number) || number <= 0;
+}
+
+function validateAnotherCalculation(anotherCalculation) {
+if (anotherCalculation.toLowerCase() === 'yes' || anotherCalculation.toLowerCase() === 'no') {
+  return false;
+}
+  return true;
+}
+function loanAmount() {
+  prompt(MESSAGES['loanAmount']);
+  let loanAmount = Number(readline.question());
+
+  while (invalidNumber(loanAmount)) {
+    prompt(MESSAGES['invalidNumber']);
+    loanAmount = Number(readline.question());
+  }
+  return loanAmount;
+}
+
+function lengthOfLoan() {
+  prompt(MESSAGES['lengthOfLoan']);
+  let lengthInYears = Number(readline.question());
+
+  while (invalidNumber(lengthInYears)
+  || !Number.isInteger(lengthInYears)) {
+    prompt(`${MESSAGES['invalidNumber']} It must be an integer.`);
+    lengthInYears = Number(readline.question());
+  }
+  return lengthInYears;
+}
+
+function interestRate() {
+  prompt(MESSAGES['interestRate']);
+  let annualInterestRate = Number(readline.question());
+
+  while (invalidNumber(annualInterestRate)) {
+    prompt(MESSAGES['invalidNumber']);
+    annualInterestRate = Number(readline.question());
+  }
+  return annualInterestRate;
+}
+
+function calculateLoan() {
+  let lengthInMonths = lengthOfLoan() * 12;
+
+  let monthlyInterestRate = interestRate() / 100 / 12;
+
+  let monthlyPayment = loanAmount() * (monthlyInterestRate /
+  (1 - Math.pow((1 + monthlyInterestRate),(-lengthInMonths))));
+
+  monthlyPayment = monthlyPayment.toFixed(2);
+  return prompt(`${MESSAGES['result']}${monthlyPayment}`);
+}
+
+function anotherCalculation() {
+  prompt(MESSAGES['anotherCalculation']);
+  let anotherCalculation = readline.question();
+
+  while (validateAnotherCalculation(anotherCalculation)) {
+    prompt(MESSAGES['validateAnotherCalculation']);
+    anotherCalculation = readline.question();
+  }
+  if (anotherCalculation.toLowerCase() === 'yes') {
+    console.clear();
+    return startCalculator();
+  }
+  return prompt('GoodBye!');
 }
 
 prompt(MESSAGES['welcome']);
 
-while (true) {
-  prompt(MESSAGES['loanAmount']);
-  let loanAmount = readline.question();
+startCalculator();
 
-  while (invalidNumber(loanAmount)) {
-    prompt(MESSAGES['invalidNumber']);
-    loanAmount = readline.question();
-  }
-
-  prompt(MESSAGES['lengthOfLoan']);
-  let lengthOfLoaninYears = readline.question();
-
-  while (invalidNumber(lengthOfLoaninYears)) {
-    prompt(MESSAGES['invalidNumber']);
-    lengthOfLoaninYears = readline.question();
-  }
-
-  prompt(MESSAGES['interestRate']);
-  let annualInterestRate = readline.question();
-
-  while (invalidNumber(annualInterestRate)) {
-    prompt(MESSAGES['interestRate']);
-    annualInterestRate = readline.question();
-  }
-  let lengthOfLoaninMonths = Number(lengthOfLoaninYears) * 12;
-
-  let monthlyInterestRate = (Number(annualInterestRate) / 100 ) / 12;
-
-  let monthlyPayment = Number(loanAmount) * (monthlyInterestRate /
-  (1 - Math.pow((1 + monthlyInterestRate),(-lengthOfLoaninMonths))));
-
-  monthlyPayment = monthlyPayment.toFixed(2);
-  prompt(`${MESSAGES['result']}${monthlyPayment}`);
-
-  prompt(MESSAGES['anotherOperation']);
-  let anotherCalculation = readline.question();
-
-  if (anotherCalculation[0].toLowerCase() !== 'y') {
-    return;
-  }
+function startCalculator() {
+  calculateLoan();
+  anotherCalculation();
 }
 
