214658f89c443ec166c1d237281e392593a75670
Blanca Hernandez <blamca1029@gmail.com>
2020-10-29 08:29:31 -0700

    made changes to twenty_one.js according to code review

--- lesson_6/twenty_one.js
+++ lesson_6/twenty_one.js
@@ -1,214 +1,223 @@
 const readline = require("readline-sync");
-const CARD_VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-const SUITS = ['H', 'D', 'C', 'S'];
-let CARDS;
-let PLAYER_CARDS;
-let DEALER_CARDS;
-let PLAYER_TOTAL;
-let DEALER_TOTAL;
-const ANSWERS_TO_HIT_OR_STAY = ["hit", "stay"];
-const ANSWERS_TO_PLAY_AGAIN = ["yes", "no"];
+
+const SUITS = ['hearts', 'diamonds', 'clubs', 'spades'];
+const VALUE = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+const ANSWERS_TO_HIT_OR_STAY = ["hit", "stay", "h", "s"];
+const ANSWERS_TO_PLAY_AGAIN = ["yes", "no", "y", "n"];
+const BLACK_JACK = 21;
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
 }
 
-//initialize deck by combining each CARD_VALUE with each SUIT
-function initializeDeck() {
+function initializeDeck(cards) {
   SUITS.forEach(suit => {
-    return CARD_VALUES.forEach(card => CARDS.push(card + suit));
+    return VALUE.forEach(value => cards.push({suit: suit, value: value}));
 });
-  return CARDS;
+  return cards;
 }
 
-function shuffle() {
-  for (let index = CARDS.length - 1; index > 0; index--) {
+function shuffle(array) {
+  for (let index = array.length - 1; index > 0; index--) {
     let otherIndex = Math.floor(Math.random() * (index + 1)); // 0 to index
-    [CARDS[index], CARDS[otherIndex]] = [CARDS[otherIndex], CARDS[index]]; // swap elements
+    [array[index], array[otherIndex]] = [array[otherIndex], array[index]]; // swap elements
   }
 }
 
-function dealCard(toWho) {
-  toWho.push(CARDS[0]);
-  CARDS.splice(0, 1);
+function dealCard(toWho, cards) {
+  toWho.push(cards[0]);
+  cards.splice(0, 1);
 }
 
-function initialDeal() {
-  dealCard(PLAYER_CARDS);
-  dealCard(PLAYER_CARDS);
-  PLAYER_TOTAL = total(PLAYER_CARDS);
-  dealCard(DEALER_CARDS);
-  dealCard(DEALER_CARDS);
-  DEALER_TOTAL = total(DEALER_CARDS);
+function initialDeal(playerCards, dealerCards, cards) {
+  dealCard(playerCards, cards);
+  dealCard(playerCards, cards);
+  dealCard(dealerCards, cards);
+  dealCard(dealerCards, cards);
 }
 
-//Card value 
-function valueOfCard(card) {
-  let value;
-  if (card.length === 3) {
-    value = 10;
-    return value;
+function initialBlackJack(playerTotal) {
+  if (playerTotal === BLACK_JACK) {
+    return true;
   }
-  if (Number(card[0])) {
-    value = Number(card[0]);
-    return value;
-  }
-  if (card[0] === 'A') {
-    value = 11;
-    return 11;
-  }
-  value = 10;
-  return value;
+  return false;
+}
+function displayPlayerGotLucky() {
+  prompt(`You got lucky and got 21 of first deal!`);
+  prompt(`You win!`);
 }
 
-function total(cards) {
-  let values = cards.map(card => valueOfCard(card));
+function displayBothBlackjack() {
+  prompt(`You got lucky and got 21 of first deal!`);
+  prompt(`However, dealer also got 21. It's a tie!`);
+}
+
+function total(xCards) {
+  let values = xCards.map(card => card['value']);
 
   let sum = 0;
   values.forEach(value => {
-      sum += value;
+    if (value === "Ace") {
+      sum += 11;
+    } else if (['Jack', 'Queen', 'King'].includes(value)) {
+      sum += 10;
+    } else {
+      sum += Number(value);
+    }
   });
 
   // correct for Aces
-  cards.filter(card => card[0] === "A").forEach(_ => {
-    if (sum > 21) sum -= 10;
+  values.filter(value => value === "Ace").forEach(_ => {
+    if (sum > BLACK_JACK) sum -= 10;
   });
 
   return sum;
 }
 
-
 //check if there is a bust
-function busted(who) {
-  if (who === 'player') {
-    PLAYER_TOTAL = total(PLAYER_CARDS);
-  if (PLAYER_TOTAL > 21) {
-    return true;
-  }
-  }
-  if (who === 'dealer') {
-    DEALER_TOTAL = total(DEALER_CARDS);
-  if (DEALER_TOTAL > 21) {
-    return true;
-  }
-  }
+function isBusted(xCards) {
+  return total(xCards) > BLACK_JACK;
 }
 
-function playerTurn() {
-  while (true) {
-   console.clear();
-   prompt(`You have: ${PLAYER_CARDS}.`);
-   prompt(`Dealer has: ${DEALER_CARDS[0]} and ?.`);
-   prompt(`Stay or Hit? (type "stay" or "hit")`);
-    let answer = readline.question().toLowerCase().trim();
-    while(ANSWERS_TO_HIT_OR_STAY.includes(answer) === false) {
-      prompt(`Invalid answer. Please enter "hit" or "stay"`);
-      answer = readline.question().toLowerCase();
-    }
-    if (answer === 'hit') {
-      dealCard(PLAYER_CARDS);
-    }
-    if (answer === 'stay' || busted('player')) break;
-  }
-  if (busted('player')) {
-    return 'busted';
+
+function determineWinner(playerTotal, dealerTotal) {
+  if (playerTotal === dealerTotal) {
+    prompt(`It's a tie.`);
+  } else if (playerTotal === BLACK_JACK) {
+    prompt(`You win!`);
+  } else if (dealerTotal === BLACK_JACK) {
+    prompt(`Sorry, dealer wins!`);
+  } else if (dealerTotal > playerTotal) {
+    prompt(`Sorry, dealer wins!`);
   } else {
-    console.log("You chose to stay!");
-    dealerTurn();
+    prompt(`You win!`);
   }
 }
-
-function dealerTurn() {
-  //while cardsTotal equal is less than seventeen, hit
-  while (DEALER_TOTAL < 17) {
-    dealCard(DEALER_CARDS);
-    DEALER_TOTAL = total(DEALER_CARDS);
-  }
-  if (DEALER_TOTAL > 21) {
-    return 'busted';
-  }
+function readCards(xCards) {
+  //return xCards.map(card => card['value'] + ' of ' + card['suit']);
+  return xCards.map(card => `${card['value']} of ${card['suit']} `);
 }
-
-function determineWinner() {
-  if (PLAYER_TOTAL === DEALER_TOTAL) {
-    console.log(`It's a push.`);
-  } else if (PLAYER_TOTAL === 21) {
-    console.log(`Player Wins!`);
-  } else if (DEALER_TOTAL === 21) {
-    console.log(`Dealer Wins!`);
-  } else if (DEALER_TOTAL > PLAYER_TOTAL) {
-    console.log(`Dealer Wins!`);
-  } else {
-    console.log(`Player Wins!`);
-  }
-}
-
-function displayCards() {
+function displayCards(playerCards, dealerCards, playerTotal, dealerTotal) {
+  let showPlayerCards = readCards(playerCards);
+  let showDealerCards = readCards(dealerCards);
   console.clear();
-  prompt(`Game Over`);
-  console.log(`Your Cards: ${PLAYER_CARDS}`);
-  console.log(`Dealer Cards: ${DEALER_CARDS}`);
+  prompt(`Your Cards: ${showPlayerCards} | Total ${playerTotal}`);
+  prompt(`Dealer Cards: ${showDealerCards} | Total ${dealerTotal}`);
 }
 
-//at the beginning of 21, check if initial 2 cards for player equal 21, 
-//if so, also check if delear's 2 cards equal 21
-function doInitialCardsTotal21() {
-  if (PLAYER_TOTAL === 21) {
-    if (DEALER_TOTAL === 21) {
-      displayCards();
-      prompt(`It's a push. Both you and Dealer got 21 on first deal.`);
-      return `push`;
-    }
-    displayCards();
-    prompt('Lucky, you got 21 on the first deal. You win!');
-    return `player`;
-  }
-}
 
-function initiateGame () {
-  console.clear();
-  CARDS = [];
-  PLAYER_CARDS = [];
-  DEALER_CARDS = [];
-  PLAYER_TOTAL = 0;
-  DEALER_TOTAL = 0;
-  initializeDeck();
-  shuffle();
-  initialDeal();
+function initiateGame (cards, playerCards, dealerCards) {
+  initializeDeck(cards);
+  shuffle(cards);
+  initialDeal(playerCards, dealerCards, cards);
 }
 
 function wantToPlayAgain() {
-  prompt(`Do you want to play again? (type "yes" or "no")`);
+  prompt(`Do you want to play again? (type "y" or "n")`);
+  //let answer = prompt(`Do you want to play again? (type "y" or "n")`);
   let answer = readline.question().toLowerCase().trim();
+  answer.toLowerCase().trim();
   while (ANSWERS_TO_PLAY_AGAIN.includes(answer) === false) {
-    prompt(`Invalid answer. Please enter "yes" or "no"`);
+    prompt(`Invalid answer. Please enter "y" or "n"`);
+    //answer = prompt(`Invalid answer. Please enter "y" or "n"`);
     answer = readline.question().toLowerCase();
+    answer.toLowerCase().trim();
   }
-  if (answer === 'yes') {
-    console.clear();
-    return newGame();
-  }
-  return console.log('GoodBye!');
+  return answer === 'y' || answer === 'yes';
 }
-function newGame() {
-initiateGame();
-if (doInitialCardsTotal21()) return wantToPlayAgain();
-let playerTurnResult = playerTurn();
-if (playerTurnResult === 'busted') {
-  displayCards();
-  prompt(`Sorry, you busted and lost!`);
-  wantToPlayAgain();
-} else {
-  let dealerTurnResult = dealerTurn();
-  if (dealerTurnResult === 'busted') {
-    displayCards();
-    prompt(`Dealer busted. You win!`);
-    return wantToPlayAgain();
+
+debugger;
+while (true) {
+  console.clear();
+  prompt('Welcome to Twenty One!');
+  let cards = [];
+  let playerCards = [];
+  let dealerCards = [];
+  initiateGame (cards, playerCards, dealerCards);
+
+  //check if player got Black Jack on first deal
+  let playerTotal = total(playerCards);
+  let dealerTotal = total(dealerCards);
+  if (initialBlackJack(playerTotal)) {
+    if (dealerTotal === BLACK_JACK) {
+      displayBothBlackjack();
+    } else {
+      displayPlayerGotLucky();
+    }
+    if (wantToPlayAgain()) {
+      continue;
+    } else {
+      prompt('Thanks for playing!');
+      break;
+    }
+  }
+
+  let sayPlayerCards = readCards(playerCards);
+  let sayDealerCards = readCards(dealerCards);
+  prompt(`Dealer has ${sayDealerCards[0]} and ?`);
+  prompt(`You have: ${sayPlayerCards}, for a total of ${total(playerCards)}.`);
+
+  // player turn
+  while (true) {
+    let playerTurn;
+    while (true) {
+      prompt('Would you like to (h)it or (s)tay?');
+      playerTurn = readline.question().toLowerCase().trim();
+      if (ANSWERS_TO_HIT_OR_STAY.includes(playerTurn)) break;
+      prompt("Sorry, must enter 'h' or 's'.");
+    }
+
+    if (playerTurn === 'h' || playerTurn === 'hit') {
+      dealCard(playerCards, cards);
+      let sayPlayerCards = readCards(playerCards);
+      prompt('You chose to hit!');
+      prompt(`Your cards are now: ${sayPlayerCards}`);
+      prompt(`Your total is now: ${total(playerCards)}`);
+    }
+
+    if (playerTurn === 's' || playerTurn === 'stay' || isBusted(playerCards)) break;
+  }
+
+  if (isBusted(playerCards)) {
+    prompt(`Sorry, you busted and lost!`);
+    if (wantToPlayAgain()) {
+      continue;
+    } else {
+      prompt('Thanks for playing!');
+      break;
+    }
   } else {
-    displayCards();
-    console.log(determineWinner());
-    return wantToPlayAgain();
+    prompt(`You stayed. Dealer Turn.`);
+  }
+
+  // dealer turn
+  while (total(dealerCards) < 17) {
+    prompt(`Dealer hits!`);
+    dealCard(dealerCards, cards);
+  }
+
+  if (isBusted(dealerCards)) {
+    prompt(`Dealer total is now: ${total(dealerCards)}`);
+    prompt(`Dealer busted, you win!`);
+    if (wantToPlayAgain()) {
+      continue;
+    } else {
+      prompt('Thanks for playing!');
+      break;
+    }
+  } else {
+    prompt(`Dealer stays.`);
+  }
+
+  // determine winner
+  playerTotal = total(playerCards);
+  dealerTotal = total(dealerCards);
+  displayCards(playerCards, dealerCards, playerTotal, dealerTotal);
+
+  determineWinner(playerTotal, dealerTotal);
+
+  if (!wantToPlayAgain()) {
+    prompt('Thanks for playing!');
+    break;
   }
 }
-}
-newGame();
